ad26bd343074caf0e47098fca556ced0e7decc33
Travis Nesland <tnesland@gmail.com>
2017-11-20 12:55:06 -0500

    Pull from siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Travis Nesland`,
+    description: 'Travis Nesland: A software developer from Charleston, SC',
+    keywords: [
+      'developer',
+      'software',
+      'web',
+      'app',
+      'nodejs',
+      'react',
+      'react-native',
+      'ruby',
+      'python',
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,31 +3,57 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Navbar from '../core/navbar.js'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Navbar />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = props => {
+  const { children } = props
+  const { title, description, keywords } = props.data.site.siteMetadata
+
+  return (
+    <div>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: description },
+          { name: 'keywords', content: keywords },
+        ]}
+      />
+      <Navbar />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        description: PropTypes.string.required,
+        keywords: PropTypes.arrayOf(PropTypes.string).required,
+        title: PropTypes.string.required,
+      })
+    })
+  })
 }
+
+export const query = graphql`
+  query SiteMetadataQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        keywords
+      }
+    }
+  }
+`
 
 export default TemplateWrapper
