9072be99e14c6d1b9fedf97a9a0e0d8c387ca5b7
Travis Nesland <tnesland@gmail.com>
2017-12-01 22:14:56 -0500

    Remove unused gatsby plugins and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,5 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
-    },
-    `gatsby-transformer-json`,
   ],
 }
