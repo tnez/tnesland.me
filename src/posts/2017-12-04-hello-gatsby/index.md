07007307d0d33cadda5b30058ed5e138dbca9cf9
Travis Nesland <tnesland@gmail.com>
2017-12-04 16:11:23 -0500

    Add syntax highlighting and inline images.

--- src/posts/2017-12-04-hello-gatsby/index.md
+++ src/posts/2017-12-04-hello-gatsby/index.md
@@ -5,3 +5,24 @@ title: "My First Post Using Gatsby"
 ---
 
 This is a test post. It is my first post using the [React](https://reactjs.org) based static-site generator [Gatsby](https://www.gatsbyjs.org).
+
+```javascript
+// does code formatting work?
+// syntax highlighting?
+import { addIndex, map } from 'redux'
+
+const data = [
+  'one',
+  'two',
+  'three'
+]
+
+const mapWithIndex = addIndex(map)
+const presenter = (item, idx) => `${idx}: ${item}`
+
+console.log(mapWithIndex(presenter, data))
+```
+
+Do images work well? Here is a picture of a fish:
+
+![Fish](./pietro-jeng-266042.jpg "A Fish")
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+import 'prismjs/themes/prism-solarizedlight.css'
+
 export default ({ data }) => {
   const { markdownRemark: post } = data
 
@@ -9,8 +11,8 @@ export default ({ data }) => {
       <Helmet
         title={`tnesland.me - ${post.frontmatter.title}`}
       />
-      <article className="pa4" style={{ fontSize: '1.2em' }}>
-        <h2 className="mt5 accent lh-title">{post.frontmatter.title}</h2>
+      <article className="pa4">
+        <h2 className="mt4 accent lh-title">{post.frontmatter.title}</h2>
         <div
           className="lh-copy"
           dangerouslySetInnerHTML={{ __html: post.html }}
