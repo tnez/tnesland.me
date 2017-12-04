4d007c6a88a0287764658c9b038aba4990e6f1ea
Travis Nesland <tnesland@gmail.com>
2017-12-04 14:31:44 -0500

    Generate static pages from included markdown

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const { forEach } = require('ramda')
+
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+
+  return graphql(`{
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      limit: 1000
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          html
+          id
+          frontmatter {
+            date
+            path
+            title
+          }
+        }
+      }
+    }
+  }`
+  ).then(res => {
+    if(res.errors) {
+      console.log(res.errors)
+      return Promise.reject(res.errors)
+    }
+
+    forEach(({ node }) => createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {} // additional data can be passed via context
+    }), res.data.allMarkdownRemark.edges)
+  })
+}
---
+++ src/posts/2017-12-04-hello-gatsby/index.md
@@ -0,0 +1,7 @@
+---
+path: "/first-gatsby-post"
+date: "2017-12-04"
+title: "My First Post Using Gatsby"
+---
+
+This is a test post. It is my first post using the [React](https://reactjs.org) based static-site generator [Gatsby](https://www.gatsbyjs.org).
