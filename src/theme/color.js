5c93e323b028dc9381266daaa467b9f0c190da4d
Travis Nesland <tnesland@gmail.com>
2020-04-17 22:44:14 -0400

    chore: bootstrap sapper proj

--- src/theme/color.js
+++
@@ -1,8 +0,0 @@
-export default {
-  accent: '#429ea6',
-  black: '#252323',
-  light: '#f5f1ed',
-  lightgrey: '#BABEC5',
-  primary: '#153b50',
-  white: '#fbfbff',
-}
--- src/theme/index.js
+++
@@ -1,2 +0,0 @@
-export { default as color } from './color.js'
-export { default as spacing } from './spacing.js'
