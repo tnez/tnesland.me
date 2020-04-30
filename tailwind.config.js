24fa4ba4c841160f2ac7017e7e196118caaf85b9
Travis Nesland <tnesland@gmail.com>
2020-04-29 22:10:52 -0400

    chore: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = {
+  'primary-green': '#073642',
+}
+
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      colors,
+    },
+  },
+  variants: {},
+  plugins: [],
+}
