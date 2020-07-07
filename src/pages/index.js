aaf0d68f83d77055c814516476ef0a8b182dd22b
Travis Nesland <tnesland@gmail.com>
2020-07-06 21:50:20 -0400

    Convert Gatsby setup to NextJS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-const IndexPage = () => (
-  <div className="vh-100 flex items-center justify-center">
-    <div>
-      <p className="lightgrey f4 f3-m f2-l fw6 lh-copy">
-        My name is Travis<br />
-        I am a software developer<br />
-        who crafts solutions in<br />
-        ruby / python / node / react
-      </p>
+function IndexPage() {
+  return (
+    <div className="vh-100 flex items-center justify-center">
+      <div>
+        <p className="lightgrey f4 f3-m f2-l fw6 lh-copy">
+          Hello, my name is Travis.
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default IndexPage
--- src/theme/spacing.js
+++
@@ -1,6 +0,0 @@
-const base = 1.66
-
-export default {
-  base: `${base}em`,
-  half: `${base / 2}em`,
-}
